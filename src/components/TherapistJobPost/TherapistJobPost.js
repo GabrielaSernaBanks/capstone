46c30899db12cb1f855b6c763ad451c5d7db7819
GabrielaSernaBanks <gserna88@gmail.com>
2023-05-11 15:52:55 -0400

    Adding json file and styling to post-therapist

--- src/components/TherapistJobPost/TherapistJobPost.js
+++ src/components/TherapistJobPost/TherapistJobPost.js
@@ -1,27 +1,33 @@
 import './TherapistJobPost.scss';
-
-
+import cases from '../../data/cases.json';
+import React, { useState } from "react";
 
 function TherapistJobPost() {
+	const [schoolIndex, setSchoolIndex] = useState([]);
+
 	return (
-	
-		<div >
-			<section className='therapistjob'>
-				<div className='therapistjob__school'>
-					<p className='therapistjob__school-name'>Florida City Elementary </p>
-				</div>
 
-				<div className='therapistjob__date'>
-					<p>Evaluation</p>
-					<p>May 23,2023</p>
-				</div>
+		<div>
+			{cases.map((jobpost) => (
+				<section className='jobpost'>
+					<p className='jobpost__header'>{jobpost.school_name}</p>
+					<div className='jobpost__info'>
+						<p className='jobpost__type'>Evaluation</p>
+						<p className='jobpost__date'>05-23-23</p>
 
-				<article>
-					<button>View Details</button>
-				</article>
+					<section>
+						<button className='jobpost__button'>View Details</button>
+					</section>
+					</div>
 
-			</section>
+
+				</section>
+			)
+
+			)}
+
 		</div>
+
 	);
 }
 
