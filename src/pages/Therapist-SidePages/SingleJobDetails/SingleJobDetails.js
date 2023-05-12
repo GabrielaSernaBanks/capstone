433a682de9e3d36ca6b7555c6aa36f566911d206
GabrielaSernaBanks <gserna88@gmail.com>
2023-05-12 11:28:24 -0400

    Added styling to job details post page

--- src/pages/Therapist-SidePages/SingleJobDetails/SingleJobDetails.js
+++ src/pages/Therapist-SidePages/SingleJobDetails/SingleJobDetails.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import './SingleJobDetails.scss';
+import Nav from "../../../components/Nav/Nav";
+import Footer from "../../../components/Footer/Footer";
 
 function SingleJobDetails() {
 
@@ -9,33 +12,45 @@ function SingleJobDetails() {
   const item = data.find((item) => item.case_id === case_id);
 
   return (
-    <div>
-      <h2>{item.school_name}</h2>
-      <section>
-        <p>Address:</p>
-        <p>{item.school_address}</p>
+    <>
+    <Nav/>
+
+    <div className="jobdetails">
+      <header className='jobdetails__header'>
+        <h2>{item.school_name}</h2>
+      </header>
+
+      <section className="jobdetails__address">
+        <p className="jobdetails__address-header">ADDRESS:</p>
+        <p className="jobdetails__address-info">{item.school_address}</p>
       </section>
-      <section>
-        <div>
-          <p>Date Posted</p>
-          <p>{item.date_posted}</p>
+
+      <section className="jobdetails__info">
+        <div className="jobdetails__info-date">
+          <p className="jobdetails__info-date-header">DATE POSTED</p>
+          <p className="jobdetails__info-date-info">{item.date_posted}</p>
         </div>
-        <div>
-          <p>Type:</p>
-          <p>{item.type}</p>
+        <div className="jobdetails__info-type">
+          <p className="jobdetails__info-type-header">TYPE</p>
+          <p className="jobdetails__info-type-info">{item.type}</p>
         </div>
       </section>
-      <section>
-        <p>Student Information</p>
-        <p>ID: {item.student_id}</p>
-        <p>DOB: {item.student_dob}</p>
-        <p>Grade level: {item.student_grade}</p>
+
+      <section className="jobdetails__student">
+        <p className="jobdetails__student-header">STUDENT INFORMATION</p>
+        <p className="jobdetails__student-id">ID Number: {item.student_id}</p>
+        <p className="jobdetails__student-dob">Date of Birth: {item.student_dob}</p>
+        <p className="jobdetails__student-grade">Grade Level: {item.student_grade}</p>
       </section>
-      <div>
-        <button >ACCEPT JOB</button>
-        <button>GO BACK</button>
+
+      <div className="jobdetails-buttons">
+        <button className="jobdetails-buttons__accept">ACCEPT JOB</button>
+        <button className="jobdetails-buttons__goback">GO BACK</button>
       </div>
+
     </div>
+    <Footer/>
+    </>
   );
 }
 
